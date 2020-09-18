e3751215e50b3a28e7b772f345e78f6bfcbc42fe
Michael Zibell <michael_zibell@outlook.com>
2020-09-18 07:37:04 +0200

    code cleanup

--- bsdd_rest/bsdd_rest.py
+++ bsdd_rest/bsdd_rest.py
@@ -1,31 +1,29 @@
-import requests
-from bs4 import BeautifulSoup
 import logging
+import requests
+import os
+from bs4 import BeautifulSoup
 from secrets.secrets import username, password
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('RestAPI')
 
 # configuration
-concept_guid = '1yoC2q3nn6lQ0yog8mbuQa'
+concept_guid = '2VWFE0qXKHuO00025QrE$V' # guid for pset_wall_common
 concept_type = 'NEST'
-relationship_type = 'COLLECTS'
-content_type = 'application/json'
+my_relationship_type = 'COLLECTS'
+response_type = 'application/json'
 
 api_url_base = 'http://bsdd.buildingsmart.org/api/4.0'
-api_url_login = '/session/login'
-api_url_concept = f'/IfdConcept/{concept_guid}'
-api_url_concept_filter = f'/IfdConcept/filter/{concept_type}'
-api_url_concept_children = f'/IfdConcept/{concept_guid}/children/{relationship_type}'
 
-concept_type_filter = 'ifc-2X4'
+concept_filter = 'ifc-2X4'
 
 
-def get_session_id():
+def get_session_id(username, password, response_type = 'application/json'):
+    api_url_login = '/session/login'
     data = {
         'email': username,
         'password': password,
-        'content-type': content_type
+        'content-type': response_type
     }
 
     login_response = requests.post(api_url_base + api_url_login, data=data)
@@ -38,13 +36,14 @@ def get_session_id():
     return session_id
 
 
-def get_concept_by_concept_id(session_id):
+def get_concept_by_concept_id(session_id, concept_guid, response_type = 'application/json'):
+    api_url_concept = f'/IfdConcept/{concept_guid}'
     params = {
         'peregrineapisessionid': session_id,
         'guid': concept_guid
     }
     header = {
-        'content-type': content_type
+        'content-type': response_type
     }
 
     received_concept = requests.get(
@@ -56,14 +55,15 @@ def get_concept_by_concept_id(session_id):
     return received_concept
 
 
-def get_page_by_page_id(session_id, page_id):
+def get_page_by_page_id(session_id, page_id, concept_type, response_type = 'application/json'):
+    api_url_concept_filter = f'/IfdConcept/filter/{concept_type}'
     params = {
         'peregrineapisessionid': session_id,
         'concept_type': concept_type
     }
     header = {
         'Cookie' : 'peregrineapisessionid=' + session_id,
-        'Content-Type' : content_type
+        'Content-Type' : response_type
     }
 
     if page_id != 'first_page':
@@ -78,32 +78,41 @@ def get_page_by_page_id(session_id, page_id):
     return received_page
 
 
-def get_concepts_by_concept_type(session_id, filter):
-    assert isinstance(filter, dict)
+def get_concepts_by_concept_type(session_id, concept_type, response_type = 'application/json', fullname_filter = None, max_pages = None):
+    assert isinstance(fullname_filter, str)
     page_id = 'first_page'
     page_count = 0
-    all_concepts_of_type = BeautifulSoup('<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ifdconcepts></ifdconcepts>', 'html.parser')
     concept_count = 0
+    all_concepts_of_type = BeautifulSoup('<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ifdconcepts></ifdconcepts>', 'html.parser')
+
+    if fullname_filter:
+        logger.info('concept filter set to ' + fullname_filter)
+    else:
+        logger.info('no filter set')
 
     while page_id:
-        received_page = get_page_by_page_id(session_id, page_id)
+        received_page = get_page_by_page_id(session_id, page_id, concept_type)
         parsed_content = BeautifulSoup(received_page.content, 'html.parser')
 
         concepts_on_page = parsed_content.find_all('ns2:ifdconcept')
-        logger.info('found ' + str(len(concepts_on_page)) + ' concepts on page ' + str(page_count))
 
         for concept in concepts_on_page:
-            for fullname in concept.find_all('fullnames'):
-                if concept_type_filter in str(fullname):
-                    all_concepts_of_type.ifdconcepts.append(concept)
-                    concept_count += 1
+            if fullname_filter:
+                for fullname in concept.find_all('fullnames'):
+                    if fullname_filter in str(fullname) and 'Pset_' in fullname.find('name').string:
+                        all_concepts_of_type.ifdconcepts.append(concept)
+                        concept_count += 1
+            else:
+                all_concepts_of_type.ifdconcepts.append(concept)
+                concept_count += 1
 
         page_id = received_page.headers.get('Next-Page')
         page_count += 1
+        logger.info('found ' + str(len(concepts_on_page)) + ' concepts on page ' + str(page_count))
 
-        #for trial with max page-count
-        # if page_count == 2:
-        #     break
+        if max_pages:
+            if page_count == max_pages:
+                break
 
     logger.info('got ' + str(concept_count) + ' concepts by filter for export')
 
@@ -112,38 +121,41 @@ def get_concepts_by_concept_type(session_id, filter):
 
 
 
-def get_children(session_id):
+def get_children(session_id, parent_guid, relationship_type, response_type = 'application/json'):
+    api_url_concept_children = f'/IfdConcept/{parent_guid}/children/{relationship_type}'
     params = {
         'peregrineapisessionid': session_id,
-        'guid': guid,
+        'guid': parent_guid,
         'relationship_type': relationship_type
     }
     header = {
-        'Cookie' : 'peregrineapisessionid='+session_id,
-        'Content-Type' : content_type
+        'Cookie' : 'peregrineapisessionid=' + session_id,
+        'Content-Type' : response_type
     }
 
-    children = requests.get(
+    received_page = requests.get(
         api_url_base + api_url_concept_children,
         params=params,
         headers=header
     )
-    logger.debug(children.headers)
-    logger.debug(children.content)
+
+    children = BeautifulSoup(received_page.content, 'html.parser')
+
+    return children
 
 
-def write_to_xml (content, typeOfData):
-    file_path = f'./output/'
-    file_name = f'concepts_by_type_{typeOfData}.xml'
+def write_to_xml (content, output_path = os.getcwd(), file_name_extension = ''):
+    file_path = f'{output_path}'
+    print(content)
+    file_name = f'/output/concepts_{file_name_extension}.xml'
     with open(file_path + file_name, 'w', encoding='utf-8') as file:
         file.write(str(content.prettify()))
 
 
 def main():
-    session_id = get_session_id()
-    all_concepts_of_type = get_concepts_by_concept_type(session_id, concept_type_filter)
-    write_to_xml(all_concepts_of_type, concept_type)
-    # children(session_id)
+    session_id = get_session_id(username, password)
+    all_concepts_of_type = get_concepts_by_concept_type(session_id, concept_type, fullname_filter = concept_filter, max_pages = 1)
+    write_to_xml(all_concepts_of_type, file_name_extension = concept_type)
 
 
 if __name__ == "__main__":
--- bsdd_rest/csv_converter.py
+++ bsdd_rest/csv_converter.py
@@ -1,11 +1,17 @@
-from bs4 import BeautifulSoup
-import pandas as pd
 import logging
 import os
-# from bsddRestPython import session_id
+import sys
+from bs4 import BeautifulSoup
+import pandas as pd
+from bsdd_rest import get_session_id, get_concepts_by_concept_type, get_children
+from secrets.secrets import username, password
 
 dirpath = os.getcwd()
 sourcename = "output/concepts_by_type_NEST.xml"
+
+concept_type = 'NEST'
+concept_filter = 'ifc-2X4'
+my_relationship_type = 'COLLECTS'
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('CsvBuilder')
@@ -23,38 +29,132 @@ def get_data(source):
     return parsed_content
 
 def concepts_to_dataframe(content):
-    dataframe = pd.DataFrame(columns=['guid', 'ifc_name', 'source'])
-    concepts = content.find_all('ns2:ifdconcept')
+    base_tag = 'ns2:ifdconcept'
+    columns = ['guid', 'ifc_name', 'source_url']
+    dataframe = pd.DataFrame(columns=columns)
+    concepts = content.find_all(base_tag)
     for concept in concepts:
-        guid = concept.find('guid').text
-        guid = str(guid)
-        guid = guid[4:-3]
+        # guid
+        guid = concept.find('guid').string
 
+        # ifc_name
         for fullname in concept.find_all('fullnames'):
-            if 'ifc-2X4' in str(fullname.find('languagecode')):
-                ifc_name = fullname.find('name').text
-                ifc_name = ifc_name[5:-4]
+            if fullname.parent.name != base_tag:
+                pass
+            elif 'ifc-2X4' in str(fullname.find('languagecode')):
+                ifc_name = fullname.find('name').string
 
-        source = 'http://bsdd.buildingsmart.org/#concept/details/' + guid
+        # source
+        source_url = 'http://bsdd.buildingsmart.org/#concept/details/' + guid
 
-        column = pd.DataFrame([[guid, ifc_name, source]], columns=['guid', 'ifc_name', 'source'])
-        dataframe = dataframe.append(column)
+        datacolumn = pd.Series([guid, ifc_name, source_url], index=columns)
+        dataframe = dataframe.append(datacolumn, ignore_index=True)
 
-    dataframe = dataframe.set_index('guid')
     logger.info(str(len(dataframe)) + ' of ' + str(len(concepts)) + ' found concepts in dataframe')
 
     return dataframe
 
-def write_to_csv(dataframe):
-    file_path = dirpath + '/' + sourcename[0:-4] + '.csv'
-    dataframe.to_csv(file_path, sep=";")
-    logger.info('wrote content to ' + file_path)
+# def fullnames_tag_has_right_parent(tag):
+#     return tag.name('fullnames') and tag.parent('ns2:ifdconceptinrelationship')
+    # and not tag.has_attr('id')
+
+def concept_relationship_to_dataframe(content, parent_guid):
+    base_tag = 'ns2:ifdconceptinrelationship'
+    columns = ['guid', 'ifc_name', 'ifc_definition', 'source_url', 'parent_guid']
+    dataframe = pd.DataFrame(columns=columns)
+    concepts = content.find_all(base_tag)
+
+    for concept in concepts:
+        # guid
+        guid = concept.find('guid').string
+
+        # ifc_name
+        for fullname in concept.find_all('fullnames'):
+            if fullname.parent.name != base_tag:
+                pass
+            elif 'ifc-2X4' in str(fullname.find('languagecode')):
+                ifc_name = fullname.find('name').string
+
+        # ifc_definition
+        for definition in concept.find_all('definitions'):
+            if 'ifc-2X4' in str(definition.find('languagecode')):
+                ifc_definition = definition.find('description').string
+
+        # source
+        source_url = 'http://bsdd.buildingsmart.org/#concept/details/' + guid
+
+        datacolumn = pd.Series([guid, ifc_name, ifc_definition, source_url, parent_guid], index=columns)
+        dataframe = dataframe.append(datacolumn, ignore_index=True)
+
+    logger.info(str(len(dataframe)) + ' of ' + str(len(concepts)) + ' found concepts in dataframe')
+
+    return dataframe
+
+def write_to_csv(dataframe, file_name):
+    dataframe.to_csv(file_name, sep=";", index=False)
+    logger.info('wrote content to ' + file_name)
 
 
 def main():
-    content = get_data(sourcename)
-    dataframe = concepts_to_dataframe(content)
-    write_to_csv(dataframe)
+    source_mode = 'web'
+    session_id = get_session_id(username, password)
+
+
+    if source_mode == 'file':
+        df_all_concepts_of_type = pd.read_csv(f'./output/concepts_by_type_{concept_type}.csv', sep=";")
+    elif source_mode == 'web':
+        all_concepts_of_type = get_concepts_by_concept_type(session_id, concept_type, fullname_filter = concept_filter)
+        df_all_concepts_of_type = concepts_to_dataframe(all_concepts_of_type)
+        write_to_csv(df_all_concepts_of_type, f'./output/concepts_by_type_{concept_type}.csv')
+    else:
+        sys.exit('source_mode was not correct')
+
+
+
+
+    # to wrap into a function
+    df_all_children = pd.DataFrame(columns = ['guid', 'ifc_name', 'ifc_definition', 'source_url', 'parent_guid'])
+
+    for concept in all_concepts_of_type.find_all ('ns2:ifdconcept'):
+        concept_guid = concept.guid.string
+        logger.info('working on concept ' + concept_guid)
+        children_of_concept = get_children(session_id, concept_guid, my_relationship_type)
+        df_children_of_concept = concept_relationship_to_dataframe(children_of_concept, concept_guid)
+
+        df_to_edit = pd.merge(df_all_children, df_children_of_concept, on='guid')
+        with pd.option_context('display.max_columns', None):  # more options can be specified also
+            logger.info(df_to_edit)
+        if len(df_all_children) == 0 or len(df_to_edit) == 0:
+            df_all_children = df_all_children.append(df_children_of_concept, ignore_index=True)
+        else:
+            try:
+              df_to_edit['parent_guid'] = df_to_edit['parent_guid_x'].astype(str) + ', ' + df_to_edit['parent_guid_y'].astype(str)
+            except:
+              logger.debug("combination of names failed for concept " + concept_guid)
+              logger.debug(df_to_edit)
+              pass
+            list_guids_to_edit = df_to_edit['guid'].tolist()
+
+            df_to_append = df_children_of_concept[(~df_children_of_concept.guid.isin(df_to_edit.guid))]
+
+            df_all_children = df_all_children.append(df_to_append, ignore_index=True)
+            logger.info('appended ' + str(len(df_to_append)) + ' of ' + str(len(df_children_of_concept)) + ' children')
+
+            df_all_children = df_all_children.set_index('guid')
+            df_to_edit = df_to_edit.set_index('guid')
+
+            for guid in list_guids_to_edit:
+                df_all_children.loc[str(guid)].parent_guid = df_to_edit.loc[str(guid)].parent_guid
+            logger.info('edited ' + str(len(df_to_edit)) + ' of ' + str(len(df_children_of_concept)) + ' children')
+
+            df_all_children.reset_index(level=['guid'])
+
+        with pd.option_context('display.max_columns', None):  # more options can be specified also
+            logger.info(df_all_children)
+
+    write_to_csv(df_all_children, f'./output/children_of_concepts_by_type_{concept_type}.csv')
+
+
 
 
 if __name__ == "__main__":
